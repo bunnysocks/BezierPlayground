982300a597d5a6d0364dfaed1c738b3160330d2c
bunnysocks <hemansakkthivelms14@gmail.com>
2025-11-06 09:38:54 +0530

    trying out vector-math

--- BezierPlayground/Math.swift
+++ BezierPlayground/Math.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import CoreGraphics
+
+extension CGPoint {
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    static func *(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+    }
+    
+    static func /(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x / rhs.x, y: lhs.y / rhs.y)
+    }
+ }
--- BezierPlayground/ViewController.swift
+++ BezierPlayground/ViewController.swift
@@ -17,6 +17,18 @@ class ViewController: UIViewController {
         let bezierView = BezierView(frame: view.bounds)
         bezierView.backgroundColor = .white
         view.addSubview(bezierView)
+        
+        let pointA = CGPoint(x: 10, y: 20)
+        let pointB = CGPoint(x: 5, y: 15)
+                
+        let sum = pointA + pointB
+        print("Sum of points: \(sum)")
+        let diff = pointA - pointB
+        print("Sum of points: \(diff)")
+        let mul = pointA * pointB
+        print("Sum of points: \(mul)")
+        let div = pointA / pointB
+        print("Sum of points: \(div)")
 
     }
 
